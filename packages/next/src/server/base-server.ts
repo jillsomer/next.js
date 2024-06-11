69e748f332feb44a2ce79fa16c59e31885f69ad6
Janka Uryga <lolzatu2@gmail.com>
2024-06-11 13:39:26 +0200

    fix: propagate waitUntil from edge runtime sandbox

--- packages/next/src/server/base-server.ts
+++ packages/next/src/server/base-server.ts
@@ -1693,7 +1693,7 @@ export default abstract class Server<
     )
   }
 
-  private getWaitUntil(): WaitUntil | undefined {
+  protected getWaitUntil(): WaitUntil | undefined {
     const builtinRequestContext = getBuiltinRequestContext()
     if (builtinRequestContext) {
       // the platform provided a request context.
